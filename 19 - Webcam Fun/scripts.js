1b3d9e5110ec1e2c9be8c3b2e4c2e1e173ffd720
Jake Ninness <jake.ninness@gmail.com>
2017-05-09 23:11:02 +1200

    getting webcam stream putting it into canvas and can take and download photo

--- 19 - Webcam Fun/scripts.js
+++ 19 - Webcam Fun/scripts.js
@@ -3,3 +3,49 @@ const canvas = document.querySelector('.photo');
 const ctx = canvas.getContext('2d');
 const strip = document.querySelector('.strip');
 const snap = document.querySelector('.snap');
+
+function getVideo (arguments) {
+  navigator.mediaDevices.getUserMedia({ video: true, audio: false })
+    .then( localMediaStream => {
+      console.log(localMediaStream);
+      video.src = window.URL.createObjectURL(localMediaStream);
+      video.play();
+    })
+    .catch( (err) => {
+      console.error('oh No!', err);
+    })
+}
+
+function paintToCanvas() {
+  const width = video.videoWidth;
+  const height = video.videoHeight;
+
+  canvas.width = width;
+  canvas.height = height;
+
+  return setInterval( () => {
+    ctx.drawImage(video, 0, 0, width, height);
+  }, 20 );
+
+  console.log(width, height);
+
+}
+
+function takePhoto () {
+  // Played the sound
+  snap.currentTime = 0;
+  snap.play();
+
+  // take the data out of the canvas
+  const data = canvas.toDataURL('image/jpeg');
+  const link = document.createElement('a');
+  link.href = data;
+  link.setAttribute('download', 'handsome');
+  link.innerHTML = `<img src="${ data }" alt="Cool Guy"/>`;
+  strip.insertBefore(link, strip.firstChild );
+
+}
+
+getVideo();
+
+video.addEventListener('canplay', paintToCanvas);
